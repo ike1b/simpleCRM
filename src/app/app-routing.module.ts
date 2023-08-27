23720f7654bc6134152ea9e560c704811c698d6f
ike1b <ikechezona@gmail.com>
2023-08-27 00:04:54 -0400

    SimpleCRM - an angular project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoansComponent } from './loans/loans.component';
+import { LoanTypesComponent } from './loan-types/loan-types.component';
+import { AddLoansComponent } from './add-loans/add-loans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'loans',
+    component: LoansComponent
+  },
+  {
+    path: 'loan-types',
+    component: LoanTypesComponent
+  },
+  {
+    path: 'loans/add-loans',
+    component: AddLoansComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
